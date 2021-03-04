ec8c37bab7e5b6bda1cd24c923fd226cfbe72518
Hamed Farag <hamed.farag.2009@gmail.com>
2021-03-05 00:26:57 +0200

    Call getMenuData into API into Home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
+import { getMenuData } from "../../apis";
+
 export default function Home() {
-  return "Home";
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const [data, error] = await getMenuData();
+    if (error) {
+      // show messsage or toast
+    } else {
+      setMenu(data);
+    }
+  };
+
+  return JSON.stringify(menu);
 }
