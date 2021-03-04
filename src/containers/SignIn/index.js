c39ef3d959b62368cb907371a998669180b67e59
Hamed Farag <hamed.farag.2009@gmail.com>
2021-03-04 23:07:29 +0200

    Add Initial Sign In Form

--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -1,3 +1,26 @@
+import { Card, Button, Form, Divider } from "semantic-ui-react";
+import { useTranslation } from "react-i18next";
+
 export default function SignIn() {
-  return "SignIn";
+  const { t } = useTranslation();
+
+  return (
+    <Card>
+      <Card.Content header={t("SIGNIN.PAGE.TITLE")} />
+      <Card.Content>
+        <Form>
+          <Form.Field>
+            <label>{t("SIGNIN.PAGE.USERNAME")}</label>
+            <input placeholder={t("SIGNIN.PAGE.USERNAME")} />
+          </Form.Field>
+          <Form.Field>
+            <label>{t("SIGNIN.PAGE.PASSWORD")}</label>
+            <input type="password" placeholder={t("SIGNIN.PAGE.PASSWORD")} />
+          </Form.Field>
+          <Divider />
+          <Button type="submit">{t("SIGNIN.PAGE.SIGNIN")}</Button>
+        </Form>
+      </Card.Content>
+    </Card>
+  );
 }
