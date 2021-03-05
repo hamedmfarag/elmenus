b32ac743d788f3765e67c5bf5133e5261aa751b4
Hamed Farag <hamed.farag.2009@gmail.com>
2021-03-05 16:40:21 +0200

    Finalize Header Component

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,19 +1,48 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Button, Container, Menu, Icon, Label } from "semantic-ui-react";
 
 import { UserContext } from "../../../userContext";
 
 import "./styles.css";
 
 export default function Header() {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const userCTX = useContext(UserContext);
+
   return (
-    <UserContext.Consumer>
-      {({ user }) => (
-        <article className="header-container">
-          <Link to={process.env.REACT_APP_ROUTE_HOME}>Logo</Link>
-          <Link to={process.env.REACT_APP_ROUTE_ADMIN}>Admin</Link>
-          {JSON.stringify(user)}
-        </article>
-      )}
-    </UserContext.Consumer>
+    <Menu size="large">
+      <Container>
+        <Menu.Item
+          as="a"
+          active
+          onClick={() => {
+            history.push(process.env.REACT_APP_ROUTE_HOME);
+          }}
+        >
+          <img
+            alt="Logo"
+            src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/dc5b2d99-2580-11e8-add5-0242ac110011.jpg"
+          />
+        </Menu.Item>
+
+        <Menu.Item position="right">
+          <Button
+            onClick={() => history.push(process.env.REACT_APP_ROUTE_ADMIN)}
+          >
+            Admin
+          </Button>
+          {userCTX.user.id && (
+            <Label basic>
+              <Icon name="user" />
+              {t("HEADER.COMPONENT.WELCOME")}
+              {userCTX.user.name}
+            </Label>
+          )}
+        </Menu.Item>
+      </Container>
+    </Menu>
   );
 }
